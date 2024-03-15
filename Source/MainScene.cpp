fa60d2ba377cae8d1bb0e7c861b7927fe7dbd218
monoliths <monoliths-uni@outlook.com>
2024-03-16 02:31:25 +0800

    Completed MainScene Skip Logic

--- Source/MainScene.cpp
+++ Source/MainScene.cpp
@@ -25,24 +25,21 @@
 
 #include "MainScene.h"
 
+#include "GameScene.h"
+
 USING_NS_AX;
 
 // Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
+static void problemLoading(const char *filename) {
     printf("Error while loading: %s\n", filename);
     printf(
-        "Depending on how you compiled you might have to add 'Content/' in front of filenames in "
-        "MainScene.cpp\n");
+            "Depending on how you compiled you might have to add 'Content/' in front of filenames in "
+            "MainScene.cpp\n");
 }
 
 // on "init" you need to initialize your instance
-bool MainScene::init()
-{
-    //////////////////////////////
-    // 1. super init first
-    if (!Scene::init())
-    {
+bool MainScene::init() {
+    if (!Scene::init()) {
         return false;
     }
 
@@ -51,90 +48,53 @@ bool MainScene::init()
     auto safeArea = _director->getSafeAreaRect();
     auto safeOrigin = safeArea.origin;
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
     // add a "close" icon to exit the progress. it's an autorelease object
     auto closeItem = MenuItemImage::create("CloseNormal.png", "CloseSelected.png",
-        AX_CALLBACK_1(MainScene::menuCloseCallback, this));
+                                           AX_CALLBACK_1(MainScene::menuCloseCallback, this));
 
-    if (closeItem == nullptr || closeItem->getContentSize().width <= 0 || closeItem->getContentSize().height <= 0)
-    {
+    if (closeItem == nullptr || closeItem->getContentSize().width <= 0 || closeItem->getContentSize().height <= 0) {
         problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-    }
-    else
-    {
+    } else {
         float x = safeOrigin.x + safeArea.size.width - closeItem->getContentSize().width / 2;
         float y = safeOrigin.y + closeItem->getContentSize().height / 2;
         closeItem->setPosition(Vec2(x, y));
     }
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
+    // Create Start Button
+    auto startItem = MenuItemLabel::create(Label::createWithTTF("Start", "fonts/Marker Felt.ttf", 40),
+                                             AX_CALLBACK_1(MainScene::startGame, this));
+    if (startItem == nullptr) {
+        problemLoading("'startButton'");
+    } else {
+        startItem->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+
+    }
+
+    // show menu
+    auto menu = Menu::create(closeItem, startItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
 
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // Some templates (uncomment what you  need)
-    auto touchListener = EventListenerTouchAllAtOnce::create();
-    touchListener->onTouchesBegan = AX_CALLBACK_2(MainScene::onTouchesBegan, this);
-    touchListener->onTouchesMoved = AX_CALLBACK_2(MainScene::onTouchesMoved, this);
-    touchListener->onTouchesEnded = AX_CALLBACK_2(MainScene::onTouchesEnded, this);
-    _eventDispatcher->addEventListenerWithSceneGraphPriority(touchListener, this);
-
-    //auto mouseListener           = EventListenerMouse::create();
-    //mouseListener->onMouseMove   = AX_CALLBACK_1(MainScene::onMouseMove, this);
-    //mouseListener->onMouseUp     = AX_CALLBACK_1(MainScene::onMouseUp, this);
-    //mouseListener->onMouseDown   = AX_CALLBACK_1(MainScene::onMouseDown, this);
-    //mouseListener->onMouseScroll = AX_CALLBACK_1(MainScene::onMouseScroll, this);
-    //_eventDispatcher->addEventListenerWithSceneGraphPriority(mouseListener, this);
-
-    //auto keyboardListener           = EventListenerKeyboard::create();
-    //keyboardListener->onKeyPressed  = AX_CALLBACK_2(MainScene::onKeyPressed, this);
-    //keyboardListener->onKeyReleased = AX_CALLBACK_2(MainScene::onKeyReleased, this);
-    //_eventDispatcher->addEventListenerWithFixedPriority(keyboardListener, 11);
-
-
-
-    // add a label shows "Hello World"
+    // add a label shows "My Little Game Demo"
     // create and initialize a label
 
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
+    auto label = Label::createWithTTF("My Little Game Demo", "fonts/Marker Felt.ttf", 24);
+    if (label == nullptr) {
         problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
+    } else {
         // position the label on the center of the screen
         label->setPosition(
-            Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height - label->getContentSize().height));
+                Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height - label->getContentSize().height));
 
         // add the label as a child to this layer
         this->addChild(label, 1);
     }
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png"sv);
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-        auto drawNode = DrawNode::create();
-        drawNode->setPosition(Vec2(0, 0));
-        addChild(drawNode);
-
-        drawNode->drawRect(safeArea.origin + Vec2(1, 1), safeArea.origin + safeArea.size, Color4F::BLUE);
-    }
+    // draw blue border
+    auto drawNode = DrawNode::create();
+    drawNode->setPosition(Vec2(0, 0));
+    addChild(drawNode);
+    drawNode->drawRect(safeArea.origin + Vec2(1, 1), safeArea.origin + safeArea.size, Color4F::BLUE);
 
     // scheduleUpdate() is required to ensure update(float) is called on every loop
     scheduleUpdate();
@@ -142,134 +102,34 @@ bool MainScene::init()
     return true;
 }
 
-
-void MainScene::onTouchesBegan(const std::vector<ax::Touch*>& touches, ax::Event* event)
-{
-    for (auto&& t : touches)
-    {
-        AXLOG("onTouchesBegan detected, X:%f  Y:%f", t->getLocation().x, t->getLocation().y);
-    }
+void MainScene::startGame(Ref *sender) {
+    auto gameScene = utils::createInstance<GameScene>();
+    _director->pushScene(gameScene);
 }
 
-void MainScene::onTouchesMoved(const std::vector<ax::Touch*>& touches, ax::Event* event)
-{
-    for (auto&& t : touches)
-    {
-        AXLOG("onTouchesMoved detected, X:%f  Y:%f", t->getLocation().x, t->getLocation().y);
-    }
-}
+void MainScene::update(float delta) {
+    switch (_gameState) {
+        case GameState::init: {
+            _gameState = GameState::update;
+            break;
+        }
 
-void MainScene::onTouchesEnded(const std::vector<ax::Touch*>& touches, ax::Event* event)
-{
-    for (auto&& t : touches)
-    {
-        AXLOG("onTouchesEnded detected, X:%f  Y:%f", t->getLocation().x, t->getLocation().y);
-    }
-}
+        case GameState::update: {
+            break;
+        }
 
-void MainScene::onMouseDown(Event* event)
-{
-    EventMouse* e = static_cast<EventMouse*>(event);
-    AXLOG("onMouseDown detected, Key: %d", static_cast<int>(e->getMouseButton()));
-}
+        case GameState::pause: {
+        }
 
-void MainScene::onMouseUp(Event* event)
-{
-    EventMouse* e = static_cast<EventMouse*>(event);
-    AXLOG("onMouseUp detected, Key: %d", static_cast<int>(e->getMouseButton()));
-}
-
-void MainScene::onMouseMove(Event* event)
-{
-    EventMouse* e = static_cast<EventMouse*>(event);
-    AXLOG("onMouseMove detected, X:%f  Y:%f", e->getCursorX(), e->getCursorY());
-}
-
-void MainScene::onMouseScroll(Event* event)
-{
-    EventMouse* e = static_cast<EventMouse*>(event);
-    AXLOG("onMouseScroll detected, X:%f  Y:%f", e->getScrollX(), e->getScrollY());
-}
-
-void MainScene::onKeyPressed(EventKeyboard::KeyCode code, Event* event)
-{
-    AXLOG("onKeyPressed, keycode: %d", static_cast<int>(code));
-}
-
-void MainScene::onKeyReleased(EventKeyboard::KeyCode code, Event* event)
-{
-    AXLOG("onKeyReleased, keycode: %d", static_cast<int>(code));
-}
-
-void MainScene::update(float delta)
-{
-    switch (_gameState)
-    {
-    case GameState::init:
-    {
-        _gameState = GameState::update;
-        break;
-    }
-
-    case GameState::update:
-    {
-        /////////////////////////////
-        // Add your codes below...like....
-        // 
-        // UpdateJoyStick();
-        // UpdatePlayer();
-        // UpdatePhysics();
-        // ...
-        break;
-    }
-
-    case GameState::pause:
-    {
-        /////////////////////////////
-        // Add your codes below...like....
-        //
-        // anyPauseStuff()
-
-        break;
-    }
-
-    case GameState::menu1:
-    {    /////////////////////////////
-        // Add your codes below...like....
-        // 
-        // UpdateMenu1();
-        break;
-    }
-
-    case GameState::menu2:
-    {    /////////////////////////////
-        // Add your codes below...like....
-        // 
-        // UpdateMenu2();
-        break;
-    }
-
-    case GameState::end:
-    {    /////////////////////////////
-        // Add your codes below...like....
-        // 
-        // CleanUpMyCrap();
-        menuCloseCallback(this);
-        break;
-    }
+        case GameState::end: {
+        }
 
     } //switch
 }
 
-void MainScene::menuCloseCallback(Ref* sender)
-{
+void MainScene::menuCloseCallback(Ref *sender) {
     // Close the axmol game scene and quit the application
     _director->end();
-
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use
-     * _director->end() as given above,instead trigger a custom event created in RootViewController.mm
-     * as below*/
-
-     // EventCustom customEndEvent("game_scene_close_event");
-     //_eventDispatcher->dispatchEvent(&customEndEvent);
+    // EventCustom customEndEvent("game_scene_close_event");
+    //_eventDispatcher->dispatchEvent(&customEndEvent);
 }
--- Source/MainScene.h
+++ Source/MainScene.h
@@ -29,39 +29,24 @@
 #include "axmol.h"
 
 
-class MainScene : public ax::Scene
-{
-    enum class GameState
-    {
+class MainScene : public ax::Scene {
+    enum class GameState {
         init = 0,
         update,
         pause,
         end,
-        menu1,
-        menu2,
     };
-    
+
 public:
     bool init() override;
+
     void update(float delta) override;
 
-    // touch
-    void onTouchesBegan(const std::vector<ax::Touch*>& touches, ax::Event* event);
-    void onTouchesMoved(const std::vector<ax::Touch*>& touches, ax::Event* event);
-    void onTouchesEnded(const std::vector<ax::Touch*>& touches, ax::Event* event);
 
-    // mouse
-    void onMouseDown(ax::Event* event);
-    void onMouseUp(ax::Event* event);
-    void onMouseMove(ax::Event* event);
-    void onMouseScroll(ax::Event* event);
-
-    // Keyboard
-    void onKeyPressed(ax::EventKeyboard::KeyCode code, ax::Event* event);
-    void onKeyReleased(ax::EventKeyboard::KeyCode code, ax::Event* event);
+    void startGame(Ref *sender);
 
     // a selector callback
-    void menuCloseCallback(Ref* sender);
+    void menuCloseCallback(Ref *sender);
 
 private:
     GameState _gameState = GameState::init;
