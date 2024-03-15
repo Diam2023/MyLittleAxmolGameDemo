c94cec175f62413ede7554273384a90c8cd6f22f
monoliths <monoliths-uni@outlook.com>
2024-03-16 02:07:41 +0800

    Changed Display direction

--- Source/AppDelegate.cpp
+++ Source/AppDelegate.cpp
@@ -34,10 +34,10 @@
 
 USING_NS_AX;
 
-static ax::Size designResolutionSize = ax::Size(720, 1280);
-static ax::Size smallResolutionSize = ax::Size(320, 480);
-static ax::Size mediumResolutionSize = ax::Size(768, 1024);
-static ax::Size largeResolutionSize = ax::Size(1536, 2048);
+static ax::Size designResolutionSize = ax::Size(1280, 720);
+static ax::Size smallResolutionSize = ax::Size(480, 320);
+static ax::Size mediumResolutionSize = ax::Size(1024, 768);
+static ax::Size largeResolutionSize = ax::Size(2048, 1536);
 
 AppDelegate::AppDelegate() {}
 
