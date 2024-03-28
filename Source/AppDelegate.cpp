10f281a28432f1d43731a98817569b2821004518
Diam <monoliths-uni@outlook.com>
2024-03-28 14:46:18 +0800

    Renamed Project name to MyLittleAxmolGameDemo

--- Source/AppDelegate.cpp
+++ Source/AppDelegate.cpp
@@ -63,9 +63,9 @@ bool AppDelegate::applicationDidFinishLaunching()
 #if (AX_TARGET_PLATFORM == AX_PLATFORM_WIN32) || (AX_TARGET_PLATFORM == AX_PLATFORM_MAC) || \
     (AX_TARGET_PLATFORM == AX_PLATFORM_LINUX)
         glView = GLViewImpl::createWithRect(
-            "MyGameDemo", ax::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+            "MyLittleAxmolGameDemo", ax::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
-        glView = GLViewImpl::create("MyGameDemo");
+        glView = GLViewImpl::create("MyLittleAxmolGameDemo");
 #endif
         director->setGLView(glView);
     }
